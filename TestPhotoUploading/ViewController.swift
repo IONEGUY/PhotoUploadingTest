2bffeb9f1e22e7144f52780d0d6ea570e764292e
Ivan Zavadsky <i.zavadsky@itransition.com>
2020-08-31 20:57:27 +0300

    Initial Commit

---
+++ TestPhotoUploading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPhotoUploading
+//
+//  Created by MacBook on 8/31/20.
+//  Copyright © 2020 Ivan Zavadsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
